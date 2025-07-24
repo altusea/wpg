63689c47ed7dee4c9a232d0ea9695fdd90e1d014
altusea <zt.song@outlook.com>
2025-07-24 22:56:36 +0800

    build: 添加 mybatis-plus-core 依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("com.j2html:j2html:1.6.0")
     implementation("dev.failsafe:failsafe:3.3.2")
     implementation("cn.idev.excel:fastexcel:1.2.0")
+    implementation("com.baomidou:mybatis-plus-core:3.5.12")
 
     runtimeOnly("org.postgresql:postgresql:42.7.7")
 
