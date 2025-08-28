0cabea5a2eabe83622c84e1a86b73250d4451da9
altusea <zt.song@outlook.com>
2025-08-28 19:39:10 +0800

    build: 添加 joda-money 依赖

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("com.github.houbb:sensitive-word:0.27.1")
     implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
     implementation("com.j2html:j2html:1.6.0")
+    implementation("org.joda:joda-money:2.0.2")
     implementation("dev.failsafe:failsafe:3.3.2")
     implementation("cn.idev.excel:fastexcel:1.3.0")
     implementation("com.baomidou:mybatis-plus-core:3.5.12")
