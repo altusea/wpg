991f5d0d0aae29f4aa07770a8dbb09c564d73a16
altusea <zt.song@outlook.com>
2026-01-28 19:48:46 +0800

    feat(build): 添加 fesod-sheet 依赖支持 Excel 处理功能
    - 集成 org.apache.fesod:fesod-sheet:2.0.0-incubating 依赖
    - 增强 Excel 文件处理能力

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("org.joda:joda-money:2.0.3")
     implementation("dev.failsafe:failsafe:3.3.2")
     implementation("cn.idev.excel:fastexcel:1.3.0")
+    implementation("org.apache.fesod:fesod-sheet:2.0.0-incubating")
     implementation("io.netty:netty-all:4.2.9.Final")
     implementation("com.baomidou:mybatis-plus-core:3.5.16")
     implementation("net.logstash.logback:logstash-logback-encoder:9.0")
