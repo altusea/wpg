f5ecb7148c6890839bd6096d9ef64541cf2e422f
altusea <zt.song@outlook.com>
2026-03-26 22:04:46 +0800

    chore(deps): 更新 jooq-codegen 依赖版本
    - 将 org.jooq:jooq-codegen 从 3.21.0 升级到 3.21.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.kafka:spring-kafka")
-    implementation("org.jooq:jooq-codegen:3.21.0")
+    implementation("org.jooq:jooq-codegen:3.21.1")
 
     implementation("net.javacrumbs.shedlock:shedlock-provider-redis-spring:7.7.0")
     implementation("org.redisson:redisson-spring-boot-starter:4.3.0")
